0237f43dfe21fe89dc798ebc724e47bdac994282
i-am-the-slime <mark.eibes@googlemail.com>
2012-12-28 12:13:06 +0100

    Please do work now

--- Assets/Code/Game.cs
+++ Assets/Code/Game.cs
@@ -26,7 +26,7 @@ public class Game : MonoBehaviour {
 	IEnumerator Start() {
 		//Initialize the base stack first thing.
 //		CardStack big_stack = new CardStack(AbsolutePosition(3,3));
-        for (int i = 0; i < 10; i++) bigStack.Push(CreateCard("Village", "base"));
+        for (int i=0; i<10; i++) bigStack.Push(CreateCard("Village", "base"));
 		for (int i=0; i<30; i++) bigStack.Push(CreateCard("Curse", "base"));
 		for (int i=0; i<12; i++) bigStack.Push(CreateCard("Province", "common"));
 		for (int i=0; i<12; i++) bigStack.Push(CreateCard("Duchy", "common"));
